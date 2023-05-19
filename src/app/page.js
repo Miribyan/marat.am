a380c9e2e215d29c0934a0a3f44d7e0114cc7880
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-05-19 17:11:30 +0400

    my profile

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+import Main_component from "./components/main";
+export default function Home() {
+    return (
+        <main>
+          
+            <Main_component />
+        </main>
+    );
+}
