32c0b649e746ee39958ab545f6119f3a9aa307a2
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-05-24 05:28:22 +0400

    adaptive fixes

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -87,13 +87,13 @@ export default function Main_component() {
                 </nav>
                 <Dialog
                     as="div"
-                    className="lg:hidden"
+                    className="lg:hidden "
                     open={mobileMenuOpen}
                     onClose={setMobileMenuOpen}
                 >
                     <div className="fixed inset-0" />
-                    <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-2/3 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                        <div className="flex items-center justify-end">
+                    <Dialog.Panel className=" fixed inset-y-0 right-0 z-50 w-2/3 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 backdrop-blur-md">
+                        <div className="flex items-center justify-end ">
                             <button
                                 type="button"
                                 className="-m-2.5 rounded-md p-2.5 text-gray-700 "
@@ -130,10 +130,13 @@ export default function Main_component() {
             >
                 <div className="flex flex-col md:flex-row w-full items-center mt-10 bg-gray-50 shadow-md rounded-xl p-3 mx-2 ">
                     <div className="w-full flex flex-col justify-center items-center z-20">
-                        <h3 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold mb-10 leading-8  text-gray-900">
-                            {"<Hello!/>"}
-                        </h3>
-                        <div className=" animate-pulse md:mb-0">
+                        <div className="w-fit">
+                            <h3 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-black pr-5 text-4xl text-black  sm:text-5xl md:text-6xl lg:text-7xl font-semibold mb-10 leading-8  text-gray-900">
+                                {"<Hello!/>"}
+                            </h3>
+                        </div>
+
+                        <div className=" md:mb-0">
                             <p className=" text-xl sm:text-2xl leading-7 self-start -ml-5 text-yellow-100  bg-gray-800 px-2 py-1 shadow-md">
                                 {"{"}
                             </p>
@@ -407,10 +410,13 @@ export default function Main_component() {
                 >
                     text_me
                 </h2>
-                <div className="text-gray-900 flex rounded shadow-md  w-full self-center">
-                    <div className="w-full sm:w-2/3">
-                        <p className="text-sm sm:text-base md:text-xl text-center mb-5 bg-gray-50 p-1 shadow-sm">
-                            I'm always open to any suggestions
+                <div className="text-gray-900 flex rounded shadow-md  w-2/3 self-center">
+                <div className="flex bg-yellow-300 text-white text-xl  py-10 justify-center items-center">
+                        <p className="text-center -rotate-90">Lets talk</p>
+                    </div>
+                    <div className="w-full">
+                        <p className="w-full text-sm sm:text-base md:text-xl text-center mb-5 bg-gray-50 p-1 shadow-sm">
+                            I'm open to any suggestions
                         </p>
                         <div className="flex justify-around items-center">
                             <a
@@ -452,16 +458,13 @@ export default function Main_component() {
                                     width="48"
                                     height="48"
                                     viewBox="0 0 24 24"
-                                    class="fill-black "
                                 >
-                                    <path d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z" />
+                                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                                 </svg>
                             </a>
                         </div>
                     </div>
-                    <div className="flex bg-yellow-300 text-white text-xl w-1/3 py-10 justify-center items-center">
-                        <p className="text-center ">Lets talk</p>
-                    </div>
+                   
                 </div>
             </div>
             <footer className="bg-white">
@@ -471,6 +474,7 @@ export default function Main_component() {
                             <a
                                 key={item.name}
                                 href={item.href}
+                                target="_blank"
                                 className="text-gray-400 hover:text-gray-500"
                             >
                                 <span className="sr-only">{item.name}</span>
